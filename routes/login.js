47a2a3f08972c95ea0ec543d0499750ee2124e6c
uekichi <uekichi@qc.commufa.jp>
2019-02-06 13:16:09 +0000

    OAuth認証ログインとアイコン表示

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,9 @@
+'use strict';
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('login', { user: req.user});
 });
 
 module.exports = router;
--- routes/logout.js
+++ routes/logout.js
@@ -1,11 +1,10 @@
+'use strict';
 var express = require('express');
 var router = express.Router();
 
-/* GET Top page. */
 router.get('/', function(req, res, next) {
-  var pic = req.user._json.avatar_url;
-  console.log(pic);
-  res.render('index', { title: 'Express', pic: pic });
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
