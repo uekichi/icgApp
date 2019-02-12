cbf691fdea2c603da813dd14dfbf9a8aae70978b
uekichi <uekichi@qc.commufa.jp>
2019-02-12 10:45:39 +0000

    herokuの環境変数に対応

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GitHubStrategy({
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: 'http://localhost:8000/auth/github/callback'
+  callbackURL: process.env.HEROKU_URL ? process.env.HEROKU_URL + 'auth/github/callback' : 'http://localhost:8000/auth/github/callback'
 },
   function (accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
