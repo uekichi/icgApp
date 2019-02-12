d167f6c983fcbea0b562682ec16edd28ba95fe7c
uekichi <uekichi@qc.commufa.jp>
2019-02-12 10:53:08 +0000

    heroku up2回目

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GitHubStrategy({
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: process.env.HEROKU_URL ? process.env.HEROKU_URL + 'auth/github/callback' : 'http://localhost:8000/auth/github/callback'
+  callbackURL: 'https://thawing-reaches-76424.herokuapp.com/auth/github/callback' || 'http://localhost:8000/auth/github/callback'
 },
   function (accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
